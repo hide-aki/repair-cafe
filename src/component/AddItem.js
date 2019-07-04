f151e688376219e6283620ff58fe0523e3cd00e2
Kira Yang <changru.yang02@gmail.com>
2019-07-04 23:34:49 +0800

    [feat]connect google sheet API

--- src/component/AddItem.js
+++ src/component/AddItem.js
@@ -1,20 +1,135 @@
 import React, { Component } from "react";
-
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import axios from 'axios'
 
 class AddItem extends Component {
+  constructor() {
+    super()
+    this.state = {
+      modal: false,
+      addData: {
+        name: '',
+        phone: '',
+        itemdetail: '',
+        itemname: ''
+      }
+    }
+  }
+
+  handleClickOpen = () => {
+    this.setState({
+      modal: true
+    })
+  }
+
+  handleClickClose = () => {
+    this.setState({
+      modal: false
+    })
+  }
+
+  changeInput = (e) => {
+    let value = e.target.value;
+    let name = e.target.id
+    this.setState(prevState => {
+      const data = prevState.addData
+      data[name] = value
+      return {
+        addData: data
+      }
+    })
+  }
+
+  submitData = () => {
+    const name = this.state.addData.name;
+    const phone = this.state.addData.phone
+    const itemname = this.state.addData.itemname
+    const itemdetail = this.state.addData.itemdetail
+    axios({
+      method: 'post',
+      url: 'https://script.google.com/macros/s/AKfycbypbhhQ6yB6Sfvf0UGyEkAbcC_cfWeLwDdd8cVHs2oYSCZqPnE/exec',
+      data: null,
+      params: {
+        name, phone, itemname, itemdetail
+      }
+    })
+      .then(function (response) {
+        if (response == "成功") {
+          alert("成功");
+          this.setState({
+            modal: false
+          })
+        }
+      })
+  }
+
+
+
   render() {
     return (
       <div>
-        <form>
-          <h3>維修人資訊</h3>
-          <label for="name">姓名</label>
-          <input id="name" type="text" />
-          <label for="phone">電話</label>
-          <input id="phone" type="text" />
-          <label for="email">E-mail</label>
-          <input id="email" type="text" />
-          <h3>維修物品資訊</h3>
-        </form>
+        <button onClick={this.handleClickOpen}>新增維修物品</button>
+        <Dialog open={this.state.modal} aria-labelledby="form-dialog-title">
+          <DialogTitle id="form-dialog-title">維修人資訊</DialogTitle>
+          <DialogContent>
+            <TextField
+              autoFocus
+              margin="dense"
+              id="name"
+              label="姓名"
+              type="name"
+              fullWidth
+              value={this.state.value}
+              onChange={this.changeInput}
+            />
+            <TextField
+              autoFocus
+              margin="dense"
+              id="phone"
+              label="電話"
+              type="phone"
+              fullWidth
+              onChange={this.changeInput}
+            />
+          </DialogContent>
+          <DialogTitle id="form-dialog-title">維修物品資訊</DialogTitle>
+          <DialogContent>
+            <TextField
+              autoFocus
+              margin="dense"
+              id="itemname"
+              label="物品名稱"
+              type="itemname"
+              fullWidth
+              onChange={this.changeInput}
+            />
+            <TextField
+              autoFocus
+              margin="dense  multiline"
+              id="itemdetail"
+              label="物品損壞細節"
+              type="itemdetail"
+              multiline
+              fullWidth
+              rows="3"
+              onChange={this.changeInput}
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={this.handleClickClose} color="primary">
+              取消
+          </Button>
+            <Button color="primary" onClick={this.submitData}>
+              送出
+          </Button>
+          </DialogActions>
+        </Dialog>
       </div>
     )
   }
--- src/component/RepairMonitor.js
+++ src/component/RepairMonitor.js
@@ -10,8 +10,7 @@ class RepairMonitor extends Component {
   constructor() {
     super()
     this.state = {
-      items: [],
-      ref: null,
+      itemslist: [],
     }
   }
 
@@ -22,68 +21,103 @@ class RepairMonitor extends Component {
   getGoogleSheetShopList = () => {
     axios.get("https://script.google.com/macros/s/AKfycbypbhhQ6yB6Sfvf0UGyEkAbcC_cfWeLwDdd8cVHs2oYSCZqPnE/exec").then((response) => {
       const items = response.data
-      items.pop()
-      console.log(items)
       const result = items.map((item, index) => {
         return {
           number: index + 1,
-          name: item[0],
-          phone: item[1],
-          email: item[2],
-          itemname: item[3],
-          category: item[4],
-          itemdetail: item[5],
+          name: item[4],
+          phone: item[5],
+          itemname: item[1],
+          category: '',
+          itemdetail: item[2],
           itemstatus: '壞掉',
-          volunteer: ''
+          volunteer: '',
+          fixresult: '',
+          fixnote: ''
         }
       })
       this.setState({
-        items: result
+        itemslist: result
       })
     }).catch()
   }
-  addItem = () => {
 
+  up = (table) => {
+    var rows = table.getRow(1); //return row component with index of 1
+    // var rowElement = rows.getElement();
   }
 
   render() {
     const options = {
       layout: "fitData",
-      // layout: "fitColumns",
       movableRows: true,
-      width: 750
+      columnVertAlign: "bottom",
+      pagination: "local",
+      paginationSize: 20,
     };
 
     const columns = [
-      { title: "編號", field: "number", align: "center", headerSort: false, editor: "input" },
-      { title: "姓名", field: "name", align: "left", headerSort: false },
-      { title: "聯絡電話", field: "phone", align: "left", headerSort: false },
-      { title: "E-mail", field: "email", align: "left", headerSort: false },
-      { title: "物品名稱", field: "itemname", align: "center", headerSort: false },
-      { title: "物品分類", field: "category", align: "center", headerSort: false },
-      { title: "物品狀態", field: "itemstatus", align: "center", headerSort: false, editor: "select", editorParams: { values: ["壞掉", "維修中", "修好了"] } },
-      { title: "維修志工", field: "volunteer", editor: "select", align: "center", headerSort: false, editorParams: { values: ["Michael", "Kira", "Sara", "Vita"] } },
+      { rowHandle: true, formatter: "handle", headerSort: false, frozen: true, width: 30, minWidth: 30 },
+      { title: "編號", field: "number", align: "center", headerSort: true, editor: "input", width: 70 },
       {
-        title: "物品維修細節",
-        field: "itemdetail",
-        headerSort: false
+        title: "維修人資訊",
+        columns: [
+          { title: "姓名", field: "name", align: "left", headerSort: false, width: 70 },
+          { title: "聯絡電話", field: "phone", align: "left", headerSort: false, width: 100 },
+        ],
+      },
+      {
+        title: "維修物品資訊",
+        columns: [
+          { title: "物品名稱", field: "itemname", headerSort: false, width: 100 },
+          { title: "物品分類", field: "category", align: "center", width: 70, headerSort: false, editor: "select", editorParams: { values: ["木工", "家電", "裁縫"] } },
+          {
+            title: "物品損壞細節",
+            field: "itemdetail",
+            headerSort: false,
+            formatter: "textarea",
+            width: 200,
+            cellClick: function (e, cell) {
+              // var data = cell.getData()
+              console.log(cell.getRow().getData())
+            },
+          }
+        ],
+      },
+      {
+        title: "維修執行",
+        columns: [
+          {
+            title: "維修志工", field: "volunteer", editor: "select", width: 70, align: "center", headerSort: false, editorParams: {
+              values: ["Michael", "Kira", "Sara", "Vita"], cellClick: function (e, cell) {
+                // var data = cell.getData()
+                console.log(cell.getRow().getData())
+              },
+            }
+          },
+          { title: "物品狀態", field: "itemstatus", align: "center", width: 70, headerSort: false, editor: "select", editorParams: { values: ["壞掉", "維修中", "修好了"] } },
+          {
+            title: "維修紀錄", field: "fixnote", align: "center", editor: "textarea", headerSort: false, width: 200
+          },
+
+        ]
       }
-    ];
+    ]
 
     return (
       <div>
-        <h1>維修物品管理</h1>
-        <button>新增維修物品</button>
+        <h1>維修流程管理</h1>
+        <AddItem />
+        <h2>待維修物品</h2>
         <ReactTabulator
           columns={columns}
-          data={this.state.items}
+          data={this.state.itemslist}
           options={options}
           className="custom-css-class"
           tooltips={false}
           layout={"fitData"}
           sorter={"string"}
+          id="itemslist"
         />
-
       </div >
     );
   }
